27549ad999a075181c75c34b9f3398638f22d7ad
Yohei Shimomae <yohei.shimomae@gmail.com>
2013-10-18 15:51:32 -0700

    updating gh-pages

--- source/_plugins/extend_config.rb
+++ source/_plugins/extend_config.rb
@@ -7,6 +7,16 @@ module Jekyll
     def generate(site)
       time = Time.new
       site.config['build_time'] = time.strftime("%Y%m%d%H%M")
+
+      tree = YAML.load_file('./source/site-info.yml')
+      if tree
+        for node in tree
+          key = node[0]
+          val = node[1]
+          site.config[key] = val
+        end
+      end
+
     end
   end
 
